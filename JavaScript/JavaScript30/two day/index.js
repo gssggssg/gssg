6708af6eb6368ef38c080e7b1e8f507dbcd130c7
wuweijiange <490104722@qq.com>
2021-10-09 11:04:22 +0800

    fix-JavaScript30day，第二天，始终会自动转动

--- JavaScript/JavaScript30/two day/index.js
+++ JavaScript/JavaScript30/two day/index.js
@@ -8,8 +8,7 @@ const minuteHand = document.getElementById("minuteHand");
 // 秒针
 const secondHand = document.getElementById("secondHand");
 
-let data =  new Date();
-
+let data;
 
 /*
   hourHandNum  24时
@@ -17,11 +16,21 @@ let data =  new Date();
   secondHandNum 60秒
 */
 
-const hourHandNum = data.getHours()
-const minuteHandNum = data.getMinutes()
-const secondHandNum = data.getSeconds()
+//没毫秒执行一次，刷新当前时间
+setInterval(function() {
+  data = new Date()
+  turn(data)
+}, 500);
 
-hourHand.style.transform = `rotate(${hourHandNum/24 * 360}deg)`
-minuteHand.style.transform = `rotate(${minuteHandNum/60 * 360}deg)`
-secondHand.style.transform = `rotate(${secondHandNum/60 * 360}deg)`
+//让时钟转起来的函数
+
+function turn(data) {
+  const hourHandNum = data.getHours()
+  const minuteHandNum = data.getMinutes()
+  const secondHandNum = data.getSeconds()
+  hourHand.style.transform = `rotate(${hourHandNum / 24 * 360}deg)`
+  minuteHand.style.transform = `rotate(${minuteHandNum / 60 * 360}deg)`
+  secondHand.style.transform = `rotate(${secondHandNum / 60 * 360}deg)`
+}
+
 
