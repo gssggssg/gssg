caf456e59abd0bf25c2daa36671a110cfa16c52d
wuweijiange <490104722@qq.com>
2021-10-17 19:49:37 +0800

    fix-优化声反馈

--- JavaScript/JavaScript30/day01--JavaScript Drum Kit/index.js
+++ JavaScript/JavaScript30/day01--JavaScript Drum Kit/index.js
@@ -19,48 +19,57 @@ function keyDown(event) {
   switch (event.keyCode) {  // 获取当前按下键盘键的编码
     case 65:
 
-      listAudios[*].currentTime = 0
-
+      listAudios[0].currentTime = 0
       listLi[0].className = "active";
       listAudios[0].currentTime = 0;
       listAudios[0].play();
       break;
     case 83:
+      listAudios[1].currentTime = 0
       listLi[1].className = "active"
       listAudios[1].currentTime = 0;
       listAudios[1].play()
       break;
     case 68:
+      listAudios[2].currentTime = 0
       listLi[2].className = "active"
       listAudios[2].currentTime = 0;
       listAudios[2].play()
       break;
     case 70:
+      listAudios[3].currentTime = 0
       listLi[3].className = "active"
       listAudios[3].currentTime = 0;
       listAudios[3].play()
       break;
     case 71:
+      listAudios[4].currentTime = 0
       listLi[4].className = "active"
       listAudios[4].currentTime = 0;
       listAudios[4].play()
       break;
     case 72:
+      listAudios[5].currentTime = 0
       listLi[5].className = "active"
       listAudios[5].currentTime = 0;
       listAudios[5].play()
       break;
     case 74:
+      listAudios[6].currentTime = 0
       listLi[6].className = "active"
       listAudios[6].currentTime = 0;
       listAudios[6].play()
       break;
     case 75:
+      listAudios[7].currentTime = 0
+
       listLi[7].className = "active"
       listAudios[7].currentTime = 0;
       listAudios[7].play()
       break;
     case 76:
+      listAudios[8].currentTime = 0
+
       listLi[8].className = "active"
       listAudios[8].currentTime = 0;
       listAudios[8].play()
