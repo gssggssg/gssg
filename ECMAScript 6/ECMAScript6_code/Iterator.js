70c1be3dedfb2c3e4dac96f3f5c02f09adce83be
wuweijiange <490104722@qq.com>
2021-12-08 09:19:04 +0800

    fix-Optimize the code

--- ECMAScript 6/ECMAScript6_code/Iterator.js
+++ ECMAScript 6/ECMAScript6_code/Iterator.js
@@ -6,8 +6,8 @@
 
 // 通过for...of 遍历 iterator 返回 iterator.interest 的数据
 const iterator = {
-  name: "gssg",
-  age: 20,
+  name: "Tom",
+  age: 16,
   interest: ["打游戏", "听音乐", "打球", "旅游"],
   [Symbol.iterator]: function () {
     _this = this;
@@ -15,8 +15,7 @@ const iterator = {
     return {
       next() {
         if (index <= _this.interest.length) {
-          index++;
-          return { value: _this.interest[index - 1], done: false }
+          return { value: _this.interest[index++], done: false }
         }
         return { value: undefined, done: true }
       },
