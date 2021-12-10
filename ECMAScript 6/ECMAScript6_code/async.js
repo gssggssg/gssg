9817f5f8edf21bef3da29d585c12f2d530fee59f
wuweijiange <490104722@qq.com>
2021-12-10 18:06:17 +0800

    feat-Added async code

---
+++ ECMAScript 6/ECMAScript6_code/async.js
@@ -0,0 +1,35 @@
+/**
+ * This is a async example
+ * @module async/await
+ * @return promise
+ */
+
+ async function asyncFn(time) {
+  
+  await new Promise(function (resolve) {
+    setTimeout(() => {
+      console.log("第一个await");
+      resolve(); // 第一个激
+    }, time);
+  })
+
+  await new Promise(function (resolve) {
+    console.log("第二个await");
+  })
+
+  // 第二个await 后面的代码没有执行完，不会继续执行后面的代码
+  return await new Promise(function (resolve) {
+    console.log("第三个await"); // 第三个不会运行
+  })
+
+}
+
+const result = asyncFn(1000);
+console.log(result); // Promise{}
+
+/**
+ * 执行顺序
+ * Promise{}
+ * 1s 后 第一个await 
+ * 第二个await 
+ */
