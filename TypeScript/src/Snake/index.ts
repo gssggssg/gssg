3dbfd96459fb418cfecb31880662374d1be2499d
gssggssg <490104722@qq.com>
2021-12-15 23:53:49 +0800

    feat-Snake added

---
+++ TypeScript/src/Snake/index.ts
@@ -0,0 +1,8 @@
+import "./index.less"
+export const snakeBut = document.getElementById("snakeBut");
+const GluttonousSnake = document.getElementById("GluttonousSnake");
+snakeBut.onclick = () => {
+    GluttonousSnake.style.display = "flex";
+}
+
+console.log(snakeBut)
