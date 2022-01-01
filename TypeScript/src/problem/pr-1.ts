e209c3294f9da6f09e6768aba1551418a394c1d3
wuweijiange <490104722@qq.com>
2022-01-01 23:32:10 +0800

    fix-Annotation error

--- TypeScript/src/problem/pr-1.ts
+++ TypeScript/src/problem/pr-1.ts
@@ -1,16 +1,17 @@
 export const pr1 = (function (): void {
-  type User = {
-    id: number;
-    kind: string;
-  };
-  function makeCustomer<T extends User>(u: T): T {
-    // Error（TS 编译器版本：v4.4.2）
-    // Type '{ id: number; kind: string; }' is not assignable to type 'T'.
-    // '{ id: number; kind: string; }' is assignable to the constraint of type 'T', 
-    // but 'T' could be instantiated with a different subtype of constraint 'User'.
-    return {
-      id: u.id,
-      kind: 'customer'
-    }
-  }
+  // type User = {
+  //   id: number;
+  //   kind: string;
+  // };
+  // function makeCustomer<T extends User>(u: T): T {
+  //   // Error（TS 编译器版本：v4.4.2）
+  //   // Type '{ id: number; kind: string; }' is not assignable to type 'T'.
+  //   // '{ id: number; kind: string; }' is assignable to the constraint of type 'T', 
+  //   // but 'T' could be instantiated with a different subtype of constraint 'User'.
+  //   return {
+  //     id: u.id,
+  //     kind: 'customer'
+  //   }
+  // }
+  
 }())
