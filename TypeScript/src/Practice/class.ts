24413a30c61e3bd8be0e50712b796ec1bc8d14e2
wuweijiange <490104722@qq.com>
2022-01-04 16:26:58 +0800

    feat-The new class

---
+++ TypeScript/src/Practice/class.ts
@@ -0,0 +1,23 @@
+class Greeter {
+  // 静态属性
+  static cname: string = "Greeter";
+  // 成员属性
+  greeting: string;
+
+  // 构造函数 - 执行初始化操作
+  constructor(message: string) {
+    this.greeting = message;
+  }
+
+  // 静态方法
+  static getClassName() {
+    return "Class name is Greeter";
+  }
+
+  // 成员方法
+  greet() {
+    return "Hello, " + this.greeting;
+  }
+}
+
+let greeter = new Greeter("world");
