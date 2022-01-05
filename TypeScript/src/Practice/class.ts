f1867a63bff9fc00a6572159e6c7040de616f538
wuweijiange <490104722@qq.com>
2022-01-05 14:27:06 +0800

    fix-Adjust the code

--- TypeScript/src/Practice/class.ts
+++ TypeScript/src/Practice/class.ts
@@ -1,23 +1,24 @@
 class Greeter {
+
+  // 构造函数 - 执行初始化操作
+  constructor(message: string) {
+    this.greeting = message;
+  };
+
   // 静态属性
   static cname: string = "Greeter";
   // 成员属性
   greeting: string;
 
-  // 构造函数 - 执行初始化操作
-  constructor(message: string) {
-    this.greeting = message;
-  }
-
   // 静态方法
   static getClassName() {
     return "Class name is Greeter";
-  }
+  };
 
   // 成员方法
   greet() {
     return "Hello, " + this.greeting;
-  }
+  };
 }
 
 let greeter = new Greeter("world");
