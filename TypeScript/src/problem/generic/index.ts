5a8c6578a877b052af4288a428b2ce2ae0266d7b
wuweijiange <490104722@qq.com>
2022-01-08 18:09:02 +0800

    feat-Add experimental code

---
+++ TypeScript/src/problem/generic/index.ts
@@ -0,0 +1,6 @@
+import { pr1 } from "../pr-1";
+import inferGssg from "./infer"
+import keyofGssg from "./keyof";
+
+const generic = { inferGssg, keyofGssg }
+export default generic;
---
+++ TypeScript/src/problem/generic/infer.ts
@@ -0,0 +1,16 @@
+/**
+ * infer 关键字
+ */
+
+/**  
+ * 语句 let num 中，通过 let 来声明了一个变量，那怎样声明一个不确定的类型变量呢？ 
+ * 答案是使用 infer 关键字，infer R 就是声明了一个类型变量 R。 
+ */
+
+const inferGssg = ():void=>{
+  console.log(2323232323);
+}
+
+inferGssg();
+
+export default inferGssg;
