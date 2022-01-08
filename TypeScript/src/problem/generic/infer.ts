669e7239879358fb5a0534ef7b6708e785aafffe
wuweijiange <490104722@qq.com>
2022-01-08 18:15:44 +0800

    fix-Optimize the code

--- TypeScript/src/problem/generic/infer.ts
+++ TypeScript/src/problem/generic/infer.ts
@@ -4,13 +4,13 @@
 
 /**  
  * 语句 let num 中，通过 let 来声明了一个变量，那怎样声明一个不确定的类型变量呢？ 
- * 答案是使用 infer 关键字，infer R 就是声明了一个类型变量 R。 
+ * 答案是使用 infer 关键字，infer R 就是声明了一个类型变量 R。
+ * infer 的作用是让 TypeScript 自己推断，并将推断的结果存储到一个类型变量中，
+ * infer 只能用于 extends 语句中。 
  */
 
-const inferGssg = ():void=>{
-  console.log(2323232323);
-}
+const inferGssg = (function () {
 
-inferGssg();
+})();
 
 export default inferGssg;
--- TypeScript/src/problem/generic/keyof.ts
+++ TypeScript/src/problem/generic/keyof.ts
@@ -2,31 +2,27 @@
  * keyof 操作符
  */
 
-/**  
- * 语句 let num 中，通过 let 来声明了一个变量，那怎样声明一个不确定的类型变量呢？ 
- * 答案是使用 infer 关键字，infer R 就是声明了一个类型变量 R。 
- */
+const keyofGssg = (function () {
 
- const keyofGssg = {
+  enum Difficulty {
+    Easy,
+    Intermediate,
+    Hard
+  }
 
-}
+  function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+
+  let tsInfo = {
+    name: "Typescript",
+    supersetOf: "Javascript",
+    difficulty: Difficulty.Intermediate
+  }
+
+  let difficulty: Difficulty =
+    getProperty(tsInfo, 'difficulty'); // OK
+
+})();
 
 export default keyofGssg;
- enum Difficulty {
-  Easy,
-  Intermediate,
-  Hard
-}
-
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
-  return obj[key];
-}
-
-let tsInfo = {
-   name: "Typescript",
-   supersetOf: "Javascript",
-   difficulty: Difficulty.Intermediate
-}
- 
-let difficulty: Difficulty = 
-  getProperty(tsInfo, 'difficulty'); // OK
