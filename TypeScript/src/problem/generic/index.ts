76a5b4d6d02a91b6b44745a56cf5bfdad70a03b8
wuweijiange <490104722@qq.com>
2022-01-10 18:20:28 +0800

    fix-add extendsGssg

--- TypeScript/src/problem/generic/index.ts
+++ TypeScript/src/problem/generic/index.ts
@@ -1,6 +1,7 @@
 import { pr1 } from "../pr-1";
-import inferGssg from "./infer"
+import inferGssg from "./infer";
 import keyofGssg from "./keyof";
+import extendsGssg from "./extends";
 
-const generic = { inferGssg, keyofGssg }
+const generic = { inferGssg, keyofGssg, extendsGssg }
 export default generic;
