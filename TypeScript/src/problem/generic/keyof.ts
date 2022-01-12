81092202649a37fe2ae1384bf4f2a8136f82293a
wuweijiange <490104722@qq.com>
2022-01-12 13:48:00 +0800

    fix-Modify the keyof code

--- TypeScript/src/problem/generic/keyof.ts
+++ TypeScript/src/problem/generic/keyof.ts
@@ -1,27 +1,30 @@
 /**
  * keyof 操作符
+ * @return 联合类型
+ */
+
+/**
+ * keyof 操作符
+ * 该操作符可以用于获取某种类型的所有键，其返回类型是联合类型。
  */
 
 const keyofGssg = (function () {
 
-  enum Difficulty {
-    Easy,
-    Intermediate,
-    Hard
-  }
+  enum me { women, man };
 
+  // K extends keyof T 继承了 K 的类型，并将 K 类型的所有键
   function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
-  }
+  };
 
-  let tsInfo = {
-    name: "Typescript",
-    supersetOf: "Javascript",
-    difficulty: Difficulty.Intermediate
-  }
+  let meInfo = {
+    name: "GSSG",
+    age: 20,
+    sax: me.man,
+  };
 
-  let difficulty: Difficulty =
-    getProperty(tsInfo, 'difficulty'); // OK
+  // K extends keyof T 就相当于
+  let mes = getProperty(meInfo, 'name');
 
 })();
 
