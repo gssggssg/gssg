007f00abc1a982f1c0a8aabeeda1bd922bea4a81
gssggssg <490104722@qq.com>
2023-02-22 12:53:54 +0800

    feat: Add oneOf processing

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -15,14 +15,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         enforce: "pre", // 优先执行对象
@@ -30,47 +22,64 @@ module.exports = {
         options: { fix: true },
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                useBuiltIns: "usage",
-                corejs: { version: 3 },
-                targets: {
-                  chrome: "70",
-                  firefox: "50",
-                  ie: "9",
-                },
-              },
-            ],
-          ],
-        },
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        loader: "url-loader",
-        options: {
-          limit: 8 * 1024,
-          esModule: false,
-          name: "[hash:10].[ext]",
-          outputPath: "assets/images",
-        },
-      },
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-      },
-      {
-        exclude: /\.(html|css|js|json|less|jpg|png|gif)$/,
-        loader: "file-loader",
-        options: {
-          name: "[hash:10].[ext]",
-          outputPath: "assets",
-        },
+        /**
+         * oneOf 中的loader 只会匹配一个
+         * 注意：不能有两个配置处理一个文件
+         */
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"],
+          },
+          {
+            test: /\.less$/,
+            use: ["style-loader", "css-loader", "less-loader"],
+          },
+
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: { version: 3 },
+                    targets: {
+                      chrome: "70",
+                      firefox: "50",
+                      ie: "9",
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: "url-loader",
+            options: {
+              limit: 8 * 1024,
+              esModule: false,
+              name: "[hash:10].[ext]",
+              outputPath: "assets/images",
+            },
+          },
+          {
+            test: /\.html$/,
+            loader: "html-loader",
+          },
+          {
+            exclude: /\.(html|css|js|json|less|jpg|png|gif)$/,
+            loader: "file-loader",
+            options: {
+              name: "[hash:10].[ext]",
+              outputPath: "assets",
+            },
+          },
+        ],
       },
     ],
   },
